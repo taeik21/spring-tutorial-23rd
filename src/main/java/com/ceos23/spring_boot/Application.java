db2d341873245e7564fbf4e2c635f37ef12afb9a
taeik <kti1452@naver.com>
2026-03-14 20:34:40 +0900

    feat: Add HelloController and Bean inspection login in main

--- src/main/java/com/ceos23/spring_boot/Application.java
+++ src/main/java/com/ceos23/spring_boot/Application.java
@@ -14,4 +14,19 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			// Spring Boot에서 제공되는 Bean 확인
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName: beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
 }
